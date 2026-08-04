00e1d56cdbe4313c5d79ed9809ff578fb4399c43
Peter Michon <peter.michon@proton.me>
2026-08-05 01:36:32 +0200

    refactor: remove dead version file handle helpers

--- backend/src/core/sites/open-version.ts
+++
@@ -1,8 +0,0 @@
-import { versionPath } from '../../shared/paths.ts';
-
-import * as fs from 'node:fs/promises';
-
-
-export async function openVersion(user: string, domain: string, index: number): Promise<fs.FileHandle> {
-  return await fs.open(versionPath(user, domain, index));
-}
