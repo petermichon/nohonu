cc8d5a1075b4a6232f20d3fa936caf1def917d19
Peter Michon <peter.michon@proton.me>
2026-08-05 21:44:17 +0200

    refactor: expose resolveDomainAndServe as its own usecase

--- backend/src/test/setup.ts
+++ backend/src/test/setup.ts
@@ -75,6 +75,7 @@ export const sites = {
   ...(await import('../usecases/sites/reset-analytics.ts')),
   ...(await import('../usecases/sites/reset-database.ts')),
   ...(await import('../usecases/sites/save-analytics.ts')),
+  ...(await import('../usecases/sites/resolve-domain-and-serve.ts')),
   ...(await import('../usecases/sites/serve-request.ts')),
   ...(await import('../usecases/sites/toggle-site.ts')),
   ...(await import('../usecases/sites/toggle-star.ts')),
